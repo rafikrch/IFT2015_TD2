f4d9d123a10ffc4c492cb690448e06e10dc1e9fc
rafikrch <rrechache@gmail.com>
2023-02-18 00:24:04 -0500

    quelques methodes

--- src/File.java
+++ src/File.java
@@ -2,38 +2,78 @@ public class File {
     public int[] elements;
     public int start;
     public int end;
+    public int taille;
+
+    public File() {
+        elements = new int[100];
+    }
+
+    public void push(int element) {
+        elements[end] = element;
+        end = (end + 1) % elements.length;
+        taille++;
+
+    }
+
+
+    public int pop() {
+        int element = elements[start];
+        elements[start] = 0;
+        start = (start + 1) % elements.length;
+        taille--;
+        return element;
+    }
+
+    public int length() {
+        return taille;
+    }
     
-    public File(){
-        // TODO
-        return;
+    public void print() {
+        String tab = "(";
+        for (int i = 0; i < taille; i++) {
+            int index = (start + i) % elements.length;
+            tab+=elements[index];
+            if (i < taille-1) {
+                tab+=",";
+            }
+        }
+        tab +=")";
+        System.out.println(tab);
     }
+    public boolean search() {
+        for(int i = 0; i < taille; i++){
 
-    public void push(int element){
-        // TODO
-    }
-
-    public int pop(){
-        // TODO
-        return 0;
-    }
-
-    public int length(){
-        // TODO
-        return 0;
-    }
-
-    public void print(){
-        // TODO
-        return;
-    }
-
-    public boolean search(){
-        // TODO
+        }
         return false;
     }
 
-    public void remove(int value){
-        // TODO
-        return;
+    public void remove(int value) {
+        for (int i = 0; i < taille; i++) {
+            if (elements[i] == value) {
+                elements[i] = 0;
+                break;
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        File file = new File();
+        {
+            file.push(1);
+            file.push(2);
+            file.push(3);
+            file.push(4);
+            file.push(3);
+            file.push(7);
+            file.print();
+            file.remove(7);
+            file.remove(3);
+            file.print();
+            System.out.println(file.length());
+
+
+        }
+
     }
 }
--- src/Node.java
+++ src/Node.java
@@ -2,29 +2,35 @@ public class Node {
     private int value;
     private Node next = null;
 
-    public Node(int value){
+    public Node(int value) {
         this.value = value;
     }
 
-    public Node(int value, Node next){
-            this.value = value;
-            this.next = next;
+    public Node(int value, Node next) {
+        this.value = value;
+        this.next = null;
     }
 
-    public void addValue(int value){
-        if (this.next == null){
+    public void addValue(int value) {
+        if (this.next == null) {
             this.next = new Node(value);
-        }else{
+        } else {
             this.next.addValue(value);
         }
 
     }
 
-    public void addNode(Node next){
-         //TODO
-    }
+    public void addNode(Node next) {
+        if (this.next == null) {
+            this.next = next;
+        }
+            else{
+                this.next.addNode(next);
+            }
 
-    public void removeLast(){
+        }
+
+    public void removeLast() {
         if (this.next.next == null) {
             this.next = null;
         } else {
@@ -33,7 +39,7 @@ public class Node {
     }
 
 
-    public void removeValue(int value){
+    public void removeValue(int value) {
         if (this.next != null) {
             if (this.next.value == value) {
                 this.next = this.next.next;
@@ -45,41 +51,107 @@ public class Node {
     }
 
 
-public int length_iteratif(){
-        int count = 1 ;
+    public int length_iteratif() {
+        int count = 1;
         Node current = this;
-        while (current.next != null){
+        while (current.next != null) {
             count++;
-            current= current.next;
+            current = current.next;
 
         }
         return count;
     }
 
-    public int length_recursion(){
-        // TODO
-        return 0;
+    public int length_recursion() {
+        int length = 1;
+        if(next == null){
+            return length;
+        }
+        else{
+            return length + next.length_recursion();
+        }
+
     }
 
-    public int returnNlast(int nLast){
-        // TODO
-        return 0;
+    public int returnNlast(int nLast) {
+        int taille = this.length_iteratif();
+        int index = taille - nLast;
+        int count = 0;
+        Node current = this;
+        if (nLast > taille) {
+            System.out.println("erreur : L'indice est superieur a la taille du tableau.");
+        }
+        while (current.next != null) {
+            current = current.next;
+            count++;
+
+            if (count == index) {
+                return current.value;
+            }
+
+        }
+       return -1;
     }
 
+    public void print() {
+        String s = "[";
+        Node current = this;
+        while (current != null) {
+            s += current.value ;
+
+            if (current.next != null){
+                s += ",";
+            }
+            current = current.next;
+        }
+        s+="]";
+        System.out.println(s);
+
+    }
     public void addValue_ordered(int value){
-        // TODO
+            Node newNode = new Node(value);
+            if (value < value) {
+                newNode.next = this;
+                value = value;
+                return;
+            }
+            Node current = this;
+            while (current.next != null && current.next.value < value) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+
+
+    public void insertionSort() {
+        if (next == null) {
+            return; // Already sorted
+        }
+
+        Node newHead = new Node(value);
+        Node current = next;
+
+        while (current != null) {
+            Node nextNode = current.next;
+
+            if (current.value < newHead.value) {
+                current.next = newHead;
+                newHead = current;
+            } else {
+                Node temp = newHead;
+                while (temp.next != null && current.value >= temp.next.value) {
+                    temp = temp.next;
+                }
+                current.next = temp.next;
+                temp.next = current;
+            }
+
+            current = nextNode;
+        }
+
+        value = newHead.value;
+        next = newHead.next;
     }
 
-    public void insertSort(){
-        // TODO
-    }
-
-    public static void main(String[] args) {
-        Node n = new Node(0);
-        n.addValue(1);
-        n.addValue(2);
-
-        System.out.println(n.length_iteratif());
-
-    }
 }
